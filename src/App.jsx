6b90640f6ff1f1b4ee725fb45fcb9866af2056ed
Damidu00 <154224349+Damidu00@users.noreply.github.com>
2025-03-07 01:25:06 +0530

    some routes defined🫡

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from './pages/home/CV/dashboard';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/cvdashboard' element={<Dashboard/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
