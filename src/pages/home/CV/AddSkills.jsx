a0948d56996b3a071143a0159fcaf81abf23217e
Damidu00 <154224349+Damidu00@users.noreply.github.com>
2025-03-15 16:46:23 +0530

    Add skills page ui created😮‍💨

--- src/pages/home/CV/AddSkills.jsx
+++ src/pages/home/CV/AddSkills.jsx
@@ -1,9 +1,83 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { CiCirclePlus } from 'react-icons/ci'
+import { Link } from 'react-router-dom'
 
 export default function AddSkills() {
+
+  const [formFields,setFormFields] = useState([
+    {name : '', age : ''},
+  ])
+
+  const handleFormChange = ((event, index)=>{
+    let data = [...formFields]
+    data[index][event.target.name] = event.target.value
+    setFormFields(data)
+  })
+
+
+  const submit = (()=>{
+    console.log(formFields)
+  })
+
+  const addFields = (()=>{
+    let object = {
+      name: '',
+      age: ''
+    }
+    setFormFields([...formFields, object])
+  })
+
   return (
-    <div>
-        
-    </div>
+    <div className="w-[900px] mx-auto mt-10 p-6 bg-white shadow-lg rounded-lg relative">
+            <h2 className="text-3xl font-bold text-gray-800 text-center mb-6">Add Your Skills</h2>
+            <form className="space-y-4" onSubmit={submit}>
+
+            {
+              formFields.map((form,index)=>{
+                return (
+                  <div key={index} className="flex flex-col gap-4">
+                    <input type="text" name="category" placeholder="Programming *" className="w-full p-2 border rounded-lg focus:ring-2 focus:ring-gray-200" 
+                    value={form.category}
+                    onChange={event => handleFormChange(event,index)}
+                    
+                    required />
+                    <div className=' w-full pl-32 '>
+                      <input type="text" name="items" placeholder="java,c,c++ " className=" w-full p-1.5 border rounded-lg focus:ring-2 focus:ring-gray-200"
+                      value={form.items}
+                      onChange={event => handleFormChange(event,index)}
+                      
+                      />
+                    </div>
+                  </div >
+                )
+              })
+            }
+
+              
+              <div className='flex justify-end '>
+                <CiCirclePlus className='bg-gray-100 text-5xl rounded-full m-2 hover:bg-gray-300 text-blue-400 hover:text-blue-700'
+                
+                onClick={addFields}
+                
+                />
+              </div>
+              
+              
+              
+            </form>
+
+            <div className="grid grid-cols-2 gap-4">
+                <Link to=''>
+                    <button type="submit" className="w-full bg-blue-600 text-white py-3 rounded-lg font-semibold hover:bg-blue-700 transition duration-300"
+                    
+                    onClick={submit}
+
+                    >Add to CV</button>
+                </Link>
+
+                <button type="" className="w-full bg-blue-600 text-white py-3 rounded-lg font-semibold hover:bg-blue-700 transition duration-300">Cancel</button>
+
+                </div>
+        </div>
   )
 }
