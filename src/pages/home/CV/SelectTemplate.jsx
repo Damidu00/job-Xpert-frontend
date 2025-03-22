069a7dd300fb87e23342509bd7b8130b25004f00
Damidu00 <154224349+Damidu00@users.noreply.github.com>
2025-03-22 12:14:04 +0530

    created dumy cv00 to test

--- src/pages/home/CV/SelectTemplate.jsx
+++ src/pages/home/CV/SelectTemplate.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import cv00 from '../../../../public/cv00.png';
 import cv01 from '../../../../public/cv01.png';
 import cv02 from '../../../../public/cv02.png';
@@ -7,29 +7,36 @@ import cv02 from '../../../../public/cv02.png';
 export default function SelectTemplate() {
   // State to track the currently hovered image
   const [hoveredImage, setHoveredImage] = useState(null);
+  const navigate = useNavigate();
+
+  const handleCvId = (id) => {
+    navigate("/viewtemplate/" + id);
+  };
 
   return (
     <div className='bg-gray-200 w-full h-screen flex items-center justify-center'>
-      <div className='shadow-lg w-[800px] h-[600px] bg-gray-100'>
-        {/* Text Part */}
-        <div className='flex flex-col items-center justify-center'>
-          <div className='flex flex-col items-start justify-center mt-2'>
-            <h2 className='text-4xl font-bold font-mono text-blue-500'>
-              Select Your Template
-            </h2>
-            <h3>See templates and choose the correct template for your entered details</h3>
+      {/* Main Container */}
+      <div className='shadow-lg w-[1200px] h-[600px] bg-gray-100 flex'>
+        {/* Left Side: Templates */}
+        <div className='w-2/3 h-full p-5 overflow-y-auto'>
+          {/* Text Part */}
+          <div className='flex flex-col items-center justify-center mb-5'>
+            <div className='flex flex-col items-start justify-center'>
+              <h2 className='text-4xl font-bold font-mono text-blue-500'>
+                Select Your Template
+              </h2>
+              <h3>See templates and choose the correct template for your entered details</h3>
+            </div>
           </div>
-        </div>
 
-        {/* Templates */}
-        <div className='w-full h-[450px] mt-5'>
-          {/* Template Sets */}
+          {/* Templates */}
           <div className='flex flex-wrap justify-center'>
             {/* Template 1 */}
             <div
               className='bg-gray-200 w-[150px] h-[200px] cursor-pointer m-5 relative'
               onMouseEnter={() => setHoveredImage(cv00)}
               onMouseLeave={() => setHoveredImage(null)}
+              onClick={() => handleCvId("cv00")}
             >
               <img src={cv00} alt="cv00" className='w-full h-full object-cover' />
             </div>
@@ -39,6 +46,7 @@ export default function SelectTemplate() {
               className='bg-gray-200 w-[150px] h-[200px] cursor-pointer m-5 relative'
               onMouseEnter={() => setHoveredImage(cv01)}
               onMouseLeave={() => setHoveredImage(null)}
+              onClick={() => handleCvId("cv01")}
             >
               <img src={cv01} alt="cv01" className='w-full h-full object-cover' />
             </div>
@@ -48,20 +56,24 @@ export default function SelectTemplate() {
               className='bg-gray-200 w-[150px] h-[200px] cursor-pointer m-5 relative'
               onMouseEnter={() => setHoveredImage(cv02)}
               onMouseLeave={() => setHoveredImage(null)}
+              onClick={() => handleCvId("cv02")}
             >
               <img src={cv02} alt="cv02" className='w-full h-full object-cover' />
             </div>
           </div>
+        </div>
 
-          {/* Display Hovered Image */}
-          {hoveredImage && (
-            <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50'>
-              <img
-                src={hoveredImage}
-                alt="hovered-template"
-                className='max-w-[500px] max-h-[600px] shadow-lg border-4 border-white'
-              />
-            </div>
+        {/* Right Side: Hovered Image Display */}
+        <div className='w-1/3 h-full bg-gray-200 flex flex-col items-center justify-center'>
+          <h3 className='text-xl font-bold text-blue-500 mb-5'>Preview</h3>
+          {hoveredImage ? (
+            <img
+              src={hoveredImage}
+              alt="hovered-template"
+              className='max-w-[400px] max-h-[500px] shadow-lg border-4 border-white'
+            />
+          ) : (
+            <p className='text-gray-500'>Hover over a template to preview</p>
           )}
         </div>
       </div>
